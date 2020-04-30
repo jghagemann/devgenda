9577752238480b2a49a02b556525672214facc59
João Guilherme Hagemann <hagejotage@hotmail.com>
2020-04-29 23:19:04 -0300

    created first controller and test route

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,7 @@
 const Koa = require('koa');
 const Logger = require('koa-logger');
 const bodyParser = require('koa-bodyparser');
-const { config } = require('./db/connection');
+const router = require('./routes/routes');
 
 const app = new Koa();
 const logger = new Logger();
@@ -9,10 +9,10 @@ const PORT = 3333;
 
 
 app
-  .use(logger)
-  .use(bodyParser())
-
-
+.use(logger)
+.use(bodyParser())
+.use(router.routes())
+  
 const server = app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -2,13 +2,6 @@ const Router = require('koa-router');
 const router = new Router();
 const SessionController = require('../controllers/SessionController');
 
-router.get('/', async (ctx) => {
-  ctx.body = {
-    status: 'success',
-    message: 'Hello World!'
-  }
-}),
+router.get('/', SessionController.isLoggedIn)
 
-router.get('/profile/:id', SessionController.create)
-
-module.exports = routes;
+module.exports = router
