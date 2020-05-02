4f6bbe1b19510236d7f5508823fcad49781c54f3
João Guilherme Hagemann <hagejotage@hotmail.com>
2020-05-02 12:29:23 -0300

    Lint: Fix Codacy code style errors

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { ThemeProvider } from '@material-ui/styles';
-import { CssBaseline } from '@material-ui/core';
-import App from './App';
-import theme from './theme'
+import React from "react";
+import ReactDOM from "react-dom";
+import { ThemeProvider } from "@material-ui/styles";
+import { CssBaseline } from "@material-ui/core";
+import App from "./App";
+import theme from "./theme";
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
@@ -12,5 +12,5 @@ ReactDOM.render(
       <App />
     </React.StrictMode>
   </ThemeProvider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
--- frontend/src/pages/AddProject.js
+++ frontend/src/pages/AddProject.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import AddProjectCard from '../components/AddProjectCard';
+import React from "react";
+import AddProjectCard from "../components/AddProjectCard";
 
 const AddProject = () => {
   return (
     <AddProjectCard />
-  )
+  );
 };
 
 export default AddProject;
